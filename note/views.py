7927dd3defb7e1f673281c42c713750bb4d63ad5
1F10240106 <s1F102401067@iniad.org>
2024-12-13 17:06:12 +0900

    遷移後のページ作成
    遷移した後のページを作成しました。

--- note/views.py
+++ note/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 #テスト用
 def test(request):
     return render(request, 'note/note.html')
+
+def new_note(request):
+    return render(request, 'new_note/new_note.html')
+
+def be_made_note(request):
+    return render(request, 'be_made_note/be_made_note.html')
