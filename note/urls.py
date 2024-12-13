f73c7279c29f4e8809249fe0e127a99ee559b8c8
1F10240106 <s1F102401067@iniad.org>
2024-12-13 17:57:38 +0900

    遷移プログラム作成

--- note/urls.py
+++ note/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('test/', views.test),
-    path('new_note/', views.new_note),
-    path('be_made_note', views.be_made_note),
-    path('home', views.home),
+    path('new_note/', views.new_note, name='new_note'),
+    path('be_made_note/', views.be_made_note, name='be_made_note'),
+    path('home/', views.home, name='home'),
 ]
