e96b02b3b8b27dd9367ad9e5a3ac92459598dff1
wakoIniad <s1f102400606@iniad.org>
2024-12-21 20:34:55 +0900

    save

--- note/default_api_handler.py
+++ note/default_api_handler.py
@@ -38,25 +38,28 @@ class DefaultApiHandler:
     def handle(self, request, **kwargs):
         print(kwargs)
         key_query = kwargs[self.KeyQuery]
+        key_model = None
         if request.method != "PUT":
             key_model = self.models_get(self.KeyModel, pk=key_query)
+            if key_model is None: return self.constants.API_RESPONSES["MODEL_NOT_FOUND"]
         
         ref_models = { q: self.RefModels[q].objects.filter( parent_id = kwargs[q]) 
                   for q in self.usingRefs[request.method] if self.RefModels[q] is not None }
         queries = { q: kwargs[q] for q in self.usingRefs[request.method]}
-        if key_model is None: return self.constants.API_RESPONSES["MODEL_NOT_FOUND"]
 
+        data = {}
         if request.method in ["POST","PUT"]:
             data = json.loads(request.body)
 
+        kwargs = { 'model': key_model, 'data': data, 'queries': queries, 'refs': ref_models }
         if request.method == "PUT":
-            return self.on_put(data=data, queries=queries, refs=ref_models)
+            return self.on_put(**kwargs)
         elif request.method == "POST":
-            return self.on_post(model=key_model, data=data, refs=ref_models)
+            return self.on_post(**kwargs)
         elif request.method == "GET":
-            return self.on_get(model=key_model, refs=ref_models)
+            return self.on_get(**kwargs)
         elif request.method == "DELETE":
-            return self.on_delete(model=key_model, refs=ref_models)
+            return self.on_delete(**kwargs)
     
     def get_model_initialization(self, data, queries):
         return {}
@@ -64,53 +67,58 @@ class DefaultApiHandler:
     def make_id(self, ref=[]):
         return random.randint(0,100000000)
     
-    def on_put(self, data, queries, refs):
-        def process(self=self, data=data, queries=queries):
+    def on_put(self, **kwargs):
+        data, queries, *_ = kwargs
+        def process():
             model = self.KeyModel( pk=self.make_id(ref=[]), **self.get_model_initialization(data, queries))
             model.save()
-            return model
+            return model.pk
         
-        result = self.put_processer(process, data, queries, refs)
-        response = self.put_response(result, data, queries, refs)
+        result = self.put_processer(process, **kwargs)
+        response = self.put_response(result, **kwargs)
         return response
 
-    def on_post(self, model, data, refs):
-        process = partial(self.update_model, self, model, data)
+    def on_post(self, **kwargs):
+        { model: model, data: data } = kwargs
+        def process():
+            return self.update_model(model, data)
 
-        result = self.post_processer(process, model, data, refs)
-        response = self.post_response(result, model, data, refs)
+        result = self.post_processer(process, **kwargs)
+        response = self.post_response(result, **kwargs)
         return response
 
-    def on_get(self, model, refs):
-        result = self.get_processer(model.json, model, refs)
-        response = self.get_response(result, model, refs)
-        return response
-    
-    def on_delete(self, model, refs):
-        result = self.delete_processer(model.delete, model, refs)
-        response = self.get_response(result, model, refs)
+    def on_get(self, **kwargs):
+        model, *_ = kwargs
+        result = self.get_processer(model.json, **kwargs)
+        response = self.get_response(result, **kwargs)
         return response
     
-    def put_response(self, response, model, data, refs):
-        return HttpResponse(response.pk)
+    def on_delete(self, **kwargs):
+        model, *_ = kwargs
+        result = self.delete_processer(model.delete, **kwargs)
+        response = self.get_response(result, **kwargs)
+        return response
+    
+    def put_response(self, response, **kwargs):
+        return HttpResponse(response)
 
-    def post_response(self, response, model, data, refs):
+    def post_response(self, response, **kwargs):
         return self.constants.API_RESPONSES["SUCCESS"]
     
-    def delete_response(self, response, refs, model):
+    def delete_response(self, response, **kwargs):
         return self.constants.API_RESPONSES["SUCCESS"]
 
-    def get_response(self, response, refs, model):
+    def get_response(self, response, **kwargs):
         return JsonResponse(response)
     
-    def put_processer(self, process, model, data, refs):
+    def put_processer(self, process, **kwargs):
         return process()
 
-    def post_processer(self, process, model, data, refs):
-        process()
+    def post_processer(self, process, **kwargs):
+        return process()
     
-    def delete_processer(self, process, refs, model):
-        process()
+    def delete_processer(self, process, **kwargs):
+        return process()
 
-    def get_processer(self, process, refs, model):
+    def get_processer(self, process, **kwargs):
         return process()
