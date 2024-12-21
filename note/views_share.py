27d041ff5ef83bfb1b9aed0f0b85830962e0a8df
wakoIniad <s1f102400606@iniad.org>
2024-12-21 14:08:51 +0900

    [Update:] リダイレクトの安全性を向上(不確か)

--- note/views_share.py
+++ note/views_share.py
@@ -3,9 +3,11 @@ from .models import ShortURL
 from django.http import HttpResponse, Http404, JsonResponse, QueryDict
 from . import my_lib
 
+
 def shortURL_redirect(request, short_url):
     try:
         short_url = ShortURL.objects.get(pk=short_url)
-        return redirect(f"{short_url.target}/editor")
+        top_page_url = my_lib.get_top_page_url(request)
+        return redirect(f"{top_page_url}/note/{short_url.target}/editor")
     except ShortURL.DoesNotExist: 
         raise Http404("404")
