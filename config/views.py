ff56b5982650c5731d88f452dff216321f6ccb2a
wakoIniad <s1f102400606@iniad.org>
2025-01-01 14:58:32 +0900

    ランディングページを追加

--- config/views.py
+++ config/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 
 
 def redirect_view(request):
     response = redirect('note/')
-    print(type(response)) 
+    print(type(response))
     return response
+
+def landing(request):
+    return render(request, 'note/landing.html')
