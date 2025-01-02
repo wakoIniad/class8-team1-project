c6f29419df7607a16d10e25c33446cb6bef93d5b
wakoIniad <s1f102400606@iniad.org>
2025-01-02 14:23:56 +0900

    save

--- note/socketio_server.js
+++ note/socketio_server.js
@@ -13,7 +13,23 @@ io.on("connection", (socket) => {
     console.log('OOON')
     const origin = socket.request.headers.origin;
     const url = new URL(origin);
-    const roomName = url.pathname;
+    const CLIENT_NAME_SPACE = url.pathname; 
+
+    //ルームではなく、名前空間を使用
+    const clientSocket = io.of(CLIENT_NAME_SPACE);
+
+    clientSocket.use((socket, next) => {
+        //ここで認証ができる
+        next();//nextで接続を許可
+    });
+
+    // 名前空間内で接続のイベントを処理
+    clientSocket.on('connection', (clientSocket) => {
+        console.log(`${CLIENT_NAME_SPACE} に接続しました`);
+
+        clientSocket.on('message', (data) => {
+            console.log(`メッセージ: ${data}`);
+    });
 });
 
 httpServer.listen(3000);
