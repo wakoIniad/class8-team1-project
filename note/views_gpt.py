51a1db1f21629e799dc1e7aa5d063ed5dd98db67
wakoIniad <168269931+wakoIniad@users.noreply.github.com>
2025-06-04 13:41:36 +0900

    Update views_gpt.py

--- note/views_gpt.py
+++ note/views_gpt.py
@@ -6,7 +6,7 @@ from langchain.chat_models import ChatOpenAI
 from langchain.schema import AIMessage, HumanMessage, SystemMessage
 import requests
 
-OPENAI_API_KEY = 'K5jP1E2JE258_uR-I4_TwVRjZmugqQm4BCPOLtP6aRwFeAmTdkGOiEsowJQmhEPZgylNqPEOOjXAh7PHmU5vNKA'
+OPENAI_API_KEY = 'OPEN_AI_API_KEY'
 OPENAI_API_BASE = 'https://api.openai.iniad.org/api/v1'
 
 chat = ChatOpenAI(openai_api_key=OPENAI_API_KEY, openai_api_base=OPENAI_API_BASE, model_name='gpt-4o-mini', temperature=0.5)
